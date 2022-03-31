f201958f312e53bd6783d40f1dd2cf1bb2cb8298
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2022-03-31 17:45:34 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -1,6 +1,10 @@
 library(raster)
-# install.packages(rgdal")
+# install.packages("RStoolbox")
+library(RStoolbox)
+# install.packages("rgdal")
 # library(rgdal)
+install.packages("rasterdiv")
+library(rasterdiv)
 
 # setwd("~/lab/") # Linux
 setwd("C:/lab/") # Windows
@@ -60,4 +64,51 @@ dvi_dif = dvi1992 - dvi2006
 cld <- colorRampPalette(c('blue','white','red'))(100) 
 dev.off()
 plot(dvi_dif, col=cld)
+
+# day 2
+
+# Range DVI (8 bit): -255 a 255
+# Range NDVI (8 bit): -1 a 1
+
+# Range DVI (16 bit): -65535 a 65535
+# Range NDVI (16 bit): -1 a 1
+
+# Quindi l'NDVI puo' esere usata acnhe con imamgini con risoluzione radiometrica differente
+
+# NDVI 1992
+
+dvi1992 = l1992[[1]] - l1992[[2]]
+ndvi1992 = dvi1992 / (l1992[[1]] + l1992[[2]])
+# or
+# ndvi1992 = (l1992[[1]] - l1992[[2]])/(l1992[[1]] + l1992[[2]]
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot(ndvi1992, col=cl)
+
+# multiframe con il plotRGB dell'immagine sopra
+# e l'NDVI sotto
+
+par(mfrow=c(2, 1))
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plot(ndvi1992, col=cl)
+
+# 2006 
+dvi2006 = l2006[[1]] - l2006[[2]]
+ndvi2006 = dvi2006 / (l2006[[1]] + l2006[[2]])
+
+# Multiframe with NDVI1992 on top of the NDVI2006 image
+par(mfrow=c(2, 1))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
+# Automatic spectral indices by the spectralIndices from package RStoolbox
+# library(RStoolbox)
+si1992 <- spectralIndices(l1992, green=3, red=2, nir=1)
+plot(si1992, col=cl)
+
+si2006 <- spectralIndices(l2006, green=3, red=2, nir=1)
+plot(si2006, col=cl)
+
+### rasterdiv
+plot(copNDVI)
+
 
