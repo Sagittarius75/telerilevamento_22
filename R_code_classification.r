57b4daa9e711192b8f9bca899a510000741d5b73
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2022-04-21 17:57:36 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -24,6 +24,46 @@ cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(soc$map, col=cl)
 
 # set.seed R function Example: set.seed(5) to uniform random numbers in different cases
+# set.seed(5)
+
+######
+
+# day 2 Grand Canyon
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc
+
+# rosso = 1
+# verde = 2
+# blu = 3
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+
+# change the stretch to histogram stretching
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+# classification
+
+gcclass2 <- unsuperClass(gc, nClasses=2)
+gcclass2
+plot(gcclass2$map)
+
+# exercise: classify the map with 4 classes
+
+gcclass4 <- unsuperClass(gc, nClasses=4)
+plot(gcclass4$map)
+
+# compare the classified map with the original set
+
+clc <- colorRampPalette(c('yellow','red','blue','black'))(100)
+par(mfrow=c(2,1))
+plot(gcclass4$map, col=clc)
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+# windows problem with image size, possible solution
+st <- stack(gc, gcclass4$map)
+par(mfrow=c(2,1))
+plot(st)
 
 
 
