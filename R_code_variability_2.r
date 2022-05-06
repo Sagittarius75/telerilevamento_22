8a6cb4e8d719ee9c3f89943cc08943a59d6ee80e
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2022-05-06 17:42:44 +0200

    Create R_code_variability_2.r

---
+++ R_code_variability_2.r
@@ -0,0 +1,78 @@
+# R code for calculating spatial variability based on multivariate maps
+
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(patchwork)
+
+#setwd("~/lab/") # Linux
+setwd("C:/lab/") # Windows
+# setwd("/Users/name/Desktop/lab/") # Mac 
+
+siml <- brick("sentinel.png")
+
+# NIR 1
+# red
+# green
+
+ggRGB(siml, 1, 2, 3)
+ggRGB(siml, 3, 1, 2)
+
+# Exercise: calculate a PCA on the image
+
+siml_pca <- rasterPCA(siml)
+
+# $call
+# $model
+# $map
+
+# Exercise: view how much variance is explained by each component
+
+summary(siml_pca$model)
+
+ggplot
+
+g1 <- ggplot() +
+geom_raster(siml_pca$map, mapping=aes(x=x, y=y, fill=PC1)) +
+scale_fill_viridis(option = "inferno") +
+ggtitle("PC1")
+
+g1
+
+g3 <- ggplot() +
+geom_raster(siml_pca$map, mapping=aes(x=x, y=y, fill=PC3)) +
+scale_fill_viridis(option = "inferno") +
+ggtitle("PC3")
+
+g3
+
+g1+g3
+
+# Exercise: insert the secod component in the graph
+
+g2 <- ggplot() +
+geom_raster(siml_pca$map, mapping=aes(x=x, y=y, fill=PC2)) +
+scale_fill_viridis(option = "inferno") +
+ggtitle("PC2")
+
+g1+g2+g3
+
+# let's calcualte variability
+
+pc1 <- siml_pca$map[[1]]
+
+sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+
+ggplot() +
+geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer)) +
+scale_fill_viridis(option = "inferno") +
+ggtitle("Standard deviation of PC1")
+
+
+
+
+
+
+
+
+
