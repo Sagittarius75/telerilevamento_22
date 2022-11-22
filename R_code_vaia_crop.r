ed9de253af61dc52f6b930c0021fd62d0fada2ed
danilonastasi <100956845+danilonastasi@users.noreply.github.com>
2022-11-22 20:37:26 +0300

    Update R_code_vaia_crop.r

--- R_code_vaia_crop.r
+++ R_code_vaia_crop.r
@@ -7,16 +7,16 @@ library(viridis)
 
 
 # setwd("~/telerilevamento_exam/vaia") # Linux
-setwd("C:/telerilevamento_exam/vaia_crop") # Windows
+setwd("C:/telerilevamento_exam/vaia_crop_02") # Windows
 # setwd("/Users/name/Desktop/telerilevamento_exam/vaia") # Mac 
 
 vlist2018 <- list.files(pattern="T32TPS_2018")
 vimport2018 <- lapply(vlist2018, raster)
-prevaia2018res <- brick(vimport2018[[4]], vimport2018[[3]], vimport2018[[2]])
+prevaia2018 <- brick(vimport2018[[4]], vimport2018[[3]], vimport2018[[2]])
 
 vlist2019 <- list.files(pattern="T32TPS_2019")
 vimport2019 <- lapply(vlist2019, raster)
-postvaia2019res <- brick(vimport2019[[4]], vimport2019[[3]], vimport2019[[2]])
+postvaia2019 <- brick(vimport2019[[4]], vimport2019[[3]], vimport2019[[2]])
 
 # layer 1 = NIR
 # layer 2 = red
@@ -27,13 +27,13 @@ postvaia2019res <- brick(vimport2019[[4]], vimport2019[[3]], vimport2019[[2]])
 
 # cir = color infrared
 
-ggpcir2018 <- ggRGB(prevaia2018res, 1, 2, 3, stretch="lin")
-ggpcir2019 <- ggRGB(postvaia2019res, 1, 2, 3, stretch="lin")
+ggpcir2018 <- ggRGB(prevaia2018, 1, 2, 3, stretch="lin")
+ggpcir2019 <- ggRGB(postvaia2019, 1, 2, 3, stretch="lin")
 
 ggpcir2018 + ggpcir2019
 
-dvi2018 = prevaia2018res[[1]] - prevaia2018res[[2]]
-dvi2019 = postvaia2019res[[1]] - postvaia2019res[[2]]
+dvi2018 = prevaia2018[[1]] - prevaia2018[[2]]
+dvi2019 = postvaia2019[[1]] - postvaia2019[[2]]
 
 # cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
 
@@ -66,8 +66,8 @@ dvi2019 = postvaia2019res[[1]] - postvaia2019res[[2]]
 
 # let's calculate NDVI
 
-ndvi2018 = dvi2018 / (prevaia2018res[[1]] + prevaia2018res[[2]])
-ndvi2019 = dvi2019 / (postvaia2019res[[1]] + postvaia2019res[[2]])
+ndvi2018 = dvi2018 / (prevaia2018[[1]] + prevaia2018[[2]])
+ndvi2019 = dvi2019 / (postvaia2019[[1]] + postvaia2019[[2]])
 
 ggpndvi2018 <- ggplot() +
 geom_raster(ndvi2018, mapping =aes(x=x, y=y, fill=layer)) +
@@ -147,8 +147,8 @@ View(multitemporal)
 # plot(postvaia2019c$map)
 # legend(x = "topright", legend = c("1: water", "2: clouds", "3: good vegetation", "4: urban", "5: perfect vegetation"), title = ("classes 2022"))
   
-pca2018 <- rasterPCA(prevaia2018res)
-pca2019 <- rasterPCA(postvaia2019res)
+pca2018 <- rasterPCA(prevaia2018)
+pca2019 <- rasterPCA(postvaia2019)
 
 pca2018
 summary(pca2018$model)
@@ -183,8 +183,8 @@ ggtitle("SD postvaia 2019")
 
 ggpsdpc1_2018 + ggpsdpc1_2019
 
-nir2018 <- prevaia2018res[[1]]
-nir2019 <- postvaia2019res[[1]]
+nir2018 <- prevaia2018[[1]]
+nir2019 <- postvaia2019[[1]]
 
 sdnir2018 <- focal(nir2018, matrix(1/9, 3, 3), fun=sd)
 # sdnir2018 <- focal(nir2018, w=matrix(1/9, nrow=3, ncol=3), fun=sd)
